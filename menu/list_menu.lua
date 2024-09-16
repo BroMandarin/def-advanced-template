2751ef5988830fca272d5265a7613403d15f9c9c
BroMandarin <bromandarin@gmail.com>
2024-09-16 17:06:23 +0300

    feat: add helper function 'select_in_direction'

--- menu/list_menu.lua
+++ menu/list_menu.lua
@@ -7,22 +7,28 @@ function list_menu.get_elements(n)
 	for e = 1, n do
 		list_menu.elements[e] = gui.get_node("element_" .. e)
 	end
-	
+
 	list_menu.elements.n = n
 end
 
 function list_menu.select(id)
 	if id > list_menu.elements.n then
-		id = 1 
+		id = 1
 	elseif id < 1 then
 		id = list_menu.elements.n
 	end
-	
+
 	local prev_id = list_menu.selected_id
 
 	list_menu.selected_id = id
-	
+
 	return prev_id, id
+end
+
+function list_menu.select_in_direction(dir)
+	local id = list_menu.selected_id + dir
+	
+	return list_menu.select(id)
 end
 
 return list_menu
