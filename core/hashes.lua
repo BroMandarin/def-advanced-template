72d845f608433e758d9870501c66bed0aa5db7cc
BroMandarin <bromandarin@gmail.com>
2024-09-23 13:31:47 +0300

    Merge 'mapper-rewrite' into 'dev'

--- core/hashes.lua
+++ core/hashes.lua
@@ -1,12 +1,18 @@
 local hashes = {}
 
+hashes.MENU_UP = hash("menu_up")
+hashes.MENU_DOWN = hash("menu_down")
+hashes.MENU_LEFT = hash("menu_left")
+hashes.MENU_RIGHT = hash("menu_right")
+hashes.MENU_ACCEPT = hash("menu_accept")
+hashes.MENU_CANCEL = hash("menu_cancel")
+
 hashes.UP = hash("up")
 hashes.DOWN = hash("down")
 hashes.LEFT = hash("left")
 hashes.RIGHT = hash("right")
 hashes.ACTION = hash("action")
-hashes.ACCEPT = hash("accept")
-hashes.CANCEL = hash("cancel")
+
 hashes.EXIT = hash("exit")
 hashes.DEBUG = hash("debug")
 
--- core/mapper.lua
+++ core/mapper.lua
@@ -1,88 +1,84 @@
-local my_mapper = {}
+local mapper = {}
 
-my_mapper.action_to_trigger = {}
-my_mapper.trigger_to_action = {}
-my_mapper.actions = {
-	{
-		event = "up",
-		name = "Up"
+mapper.action_to_trigger = {}
+mapper.trigger_to_action = { menu = {}, game = {} }
+mapper.actions = {
+	menu = {
+		{ event = "menu_up",     name = "Up" },
+		{ event = "menu_down",   name = "Down" },
+		{ event = "menu_left",   name = "Left" },
+		{ event = "menu_right",  name = "Right" },
+
+		{ event = "menu_accept", name = "Accept" },
+		{ event = "menu_cancel", name = "Cancel" },
 	},
-	{
-		event = "down",
-		name = "Down"
-	},
-	{
-		event = "left",
-		name = "Left"
-	},
-	{
-		event = "right",
-		name = "Right"
-	},
-	{
-		event = "accept",
-		name = "Accept"
-	},
-	{
-		event = "cancel",
-		name = "Cancel"
+	game = {
+		{ event = "up",     name = "Up" },
+		{ event = "down",   name = "Down" },
+		{ event = "left",   name = "Left" },
+		{ event = "right",  name = "Right" },
+
+		{ event = "action", name = "Action" },
 	}
 }
 
 local function unbind(trigger)
-	my_mapper.trigger_to_action[trigger] = nil
+	mapper.trigger_to_action[trigger] = nil
 end
 
-function my_mapper.bind(action, trigger)
-	if my_mapper.action_to_trigger[action] then
-		unbind(my_mapper.action_to_trigger[action])
+function mapper.bind(action, trigger, group)
+	print("bind", action, trigger, group)
+	if mapper.action_to_trigger[action] then
+		unbind(mapper.action_to_trigger[action])
 	end
-	my_mapper.action_to_trigger[action] = trigger
-	my_mapper.trigger_to_action[trigger] = action
+	mapper.action_to_trigger[action] = trigger
+	mapper.trigger_to_action[group][trigger] = action
 end
 
-function my_mapper.on_input(trigger)
+function mapper.on_input(trigger, group)
 	if not trigger then return nil end
+	
+	if not group then group = "menu" end
 
-	local binding = my_mapper.trigger_to_action[trigger]
+	local binding = mapper.trigger_to_action[group][trigger]
 
 	if not binding then return nil end
 
 	return binding
 end
 
-function my_mapper.save_to_file()
-	pprint(my_mapper.action_to_trigger)
+function mapper.save_to_file()
+	pprint(mapper.action_to_trigger)
 	local file = sys.get_save_file("advanced_template", "input")
 	local table = {}
 
-	for k, v in ipairs(my_mapper.actions) do
+	for k, v in ipairs(mapper.actions) do
 		print(k, v.event)
-		print(my_mapper.action_to_trigger[hash(v.event)])
-		table[v.event] = my_mapper.action_to_trigger[hash(v.event)]
+		print(mapper.action_to_trigger[hash(v.event)])
+		table[v.event] = mapper.action_to_trigger[hash(v.event)]
 	end
 
 	sys.save(file, table)
 	pprint(table)
 end
 
-function my_mapper.load_from_file()
+function mapper.load_from_file()
 	local file = sys.get_save_file("advanced_template", "input")
 	local table = {}
-	
+
 	if not sys.exists(file) then return end
-	
+
 	table = sys.load(file)
-	
+
 	pprint(table)
-	
+
 	for k, v in pairs(table) do
 		print(k, v)
-		my_mapper.bind(hash(k), v)
+		mapper.bind(hash(k), v)
 		-- my_mapper.action_to_trigger[hash(k)] = v
 	end
-	
-	pprint(my_mapper.action_to_trigger)
+
+	pprint(mapper.action_to_trigger)
 end
 
-return my_mapper
+return mapper
